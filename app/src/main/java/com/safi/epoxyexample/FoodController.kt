a415354e57916932d3dfe14c998bc2ee5cfade46
Safi <ensahinsafi@gmai.com>
2023-06-13 23:26:39 +0600

    add image

--- app/src/main/java/com/safi/epoxyexample/FoodController.kt
+++ app/src/main/java/com/safi/epoxyexample/FoodController.kt
@@ -3,18 +3,6 @@ package com.safi.epoxyexample
 import com.airbnb.epoxy.EpoxyController
 
 class FoodController : EpoxyController() {
-
-    private val foodList = listOf(
-        FoodEntity(image = R.drawable.ic_launcher_background, title = "Pizza", description = "Burger"),
-        FoodEntity(image = R.drawable.ic_launcher_background, title = "Pizza", description = "Burger"),
-        FoodEntity(image = R.drawable.ic_launcher_background, title = "Pizza", description = "Burger"),
-        FoodEntity(image = R.drawable.ic_launcher_background, title = "Pizza", description = "Burger"),
-        FoodEntity(image = R.drawable.ic_launcher_background, title = "Pizza", description = "Burger"),
-        FoodEntity(image = R.drawable.ic_launcher_background, title = "Pizza", description = "Burger"),
-        FoodEntity(image = R.drawable.ic_launcher_background, title = "Pizza", description = "Burger"),
-        FoodEntity(image = R.drawable.ic_launcher_background, title = "Pizza", description = "Burger")
-    )
-
     override fun buildModels() {
         var id:Long = 0
         foodList.forEach { foodEntity ->
--- app/src/main/java/com/safi/epoxyexample/FoodEntity.kt
+++ app/src/main/java/com/safi/epoxyexample/FoodEntity.kt
@@ -5,3 +5,21 @@ data class FoodEntity(
     val title: String,
     val description : String
 )
+
+val foodList = listOf(
+    FoodEntity(image = R.drawable.img_pizza, title = "Pizza", description = "pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese."),
+    FoodEntity(image = R.drawable.img_burger, title = "Burger", description = "A burger is a patty of ground beef grilled and placed between two halves of a bun. Slices of raw onion, lettuce, bacon, mayonnaise, and other ingredients add flavor."),
+    FoodEntity(image = R.drawable.img_sandwich, title = "Sandwich", description = "A sandwich is a food prepared by placing different kinds of foods between two pieces of bread. The pieces of bread in a sandwich are referred to as slices or sheets of bread."),
+
+    FoodEntity(image = R.drawable.img_pizza, title = "Pizza", description = "pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese."),
+    FoodEntity(image = R.drawable.img_burger, title = "Burger", description = "A burger is a patty of ground beef grilled and placed between two halves of a bun. Slices of raw onion, lettuce, bacon, mayonnaise, and other ingredients add flavor."),
+    FoodEntity(image = R.drawable.img_sandwich, title = "Sandwich", description = "A sandwich is a food prepared by placing different kinds of foods between two pieces of bread. The pieces of bread in a sandwich are referred to as slices or sheets of bread."),
+
+    FoodEntity(image = R.drawable.img_pizza, title = "Pizza", description = "pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese."),
+    FoodEntity(image = R.drawable.img_burger, title = "Burger", description = "A burger is a patty of ground beef grilled and placed between two halves of a bun. Slices of raw onion, lettuce, bacon, mayonnaise, and other ingredients add flavor."),
+    FoodEntity(image = R.drawable.img_sandwich, title = "Sandwich", description = "A sandwich is a food prepared by placing different kinds of foods between two pieces of bread. The pieces of bread in a sandwich are referred to as slices or sheets of bread."),
+
+    FoodEntity(image = R.drawable.img_pizza, title = "Pizza", description = "pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese."),
+    FoodEntity(image = R.drawable.img_burger, title = "Burger", description = "A burger is a patty of ground beef grilled and placed between two halves of a bun. Slices of raw onion, lettuce, bacon, mayonnaise, and other ingredients add flavor."),
+    FoodEntity(image = R.drawable.img_sandwich, title = "Sandwich", description = "A sandwich is a food prepared by placing different kinds of foods between two pieces of bread. The pieces of bread in a sandwich are referred to as slices or sheets of bread."),
+)
